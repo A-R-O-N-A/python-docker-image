34a33ba235f0f99d49f76548deb766f54786b3dd
franz <franzbugtong0411@gmail.com>
2025-11-27 18:13:34 +0800

    Added cowsay pip install

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM python
 WORKDIR /app
 COPY . /app
+
+
+RUN pip install cowsay
+
 CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
+import cowsay
+
 
 print('Hello to docker')
 print('I hope i can learn docker with python')
 print('The current directory is : ', os.getcwd())
 
 print('This is a new change')
+
+cowsay.cow('Good Moooooooooooooorning !')
