3b37699b01eae2e059795361e2c0be7274b81c51
franz <franzbugtong0411@gmail.com>
2025-12-01 04:15:24 +0800

    bugfix : cleaned up main.py comments

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# from typing import Union
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get('/')
-# def read_root():
-#     return {'Waifu' : 'Remilia Scarlet'}
-
-# @app.get('/items/{item_id}')
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {'item_id' : item_id, 'q' : q}
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
